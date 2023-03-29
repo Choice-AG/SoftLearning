664881e76f995a0bda2d2210130b0153eaf7e33b
Goizane <goizaneolaneta@gmail.com>
2023-03-29 23:38:41 +0200

    29-03 23:38

--- model/stakeholders/StakeHolderFunctionals.php
+++ model/stakeholders/StakeHolderFunctionals.php
@@ -7,9 +7,13 @@ include_once 'CompanyProvider.php';
 include_once 'Employee.php';
 include_once 'Provider.php';
 
+//CompanyClient
 $cc = new CompanyClient("CEFP Nuria", "08-11547", "936622113", "nuria@escolesnuria.cat", "Apel·les Mestres 52", 
     "01-09-1933", 1000, "reg mercant", "SL", 45);
-
 $cc->SetType("SA");
+
 print $cc->getName() . " : " . $cc->getType();
 print "<br>Fecha de creación: " . $cc->getBirthday();
+
+//Client
+$c = new Client("cliente", "cliente@gmail.com", "001", "678978978", "La empresa de al lado Nº5", "02-03-1978", 1);
