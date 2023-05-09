93b494d463b39fb1fa93a883a830a082aecee771
Goizane <goizaneolaneta@gmail.com>
2023-05-09 22:29:49 +0200

    09/05 22:29

--- tests/serializationDemo.php
+++ tests/serializationDemo.php
@@ -4,16 +4,10 @@ declare(strict_types=1);
 include_once '../model/stakeholders/Client.php';
 include_once '../model/checks/checker.php';
 include_once '../model/appservices/clients/JsonClientSerializer.php';
-include_once '../model/products/Libros.php';
+include_once '../model/products/Libro.php';
 include_once '../exceptions/DateException.php';
 include_once '../exceptions/BuildException.php';
 include_once '../model/appservices/book/JsonBookSerializer.php';
-
-/*
-include_once '../exceptions/DateException.php';
-include_once '../model/stakeholders/Client.php';
-
-include_once '../model/products/Interface/Storable.php'; */
 
 try {
   $client = new Client("Alvaro Salas", "012", "656656656", "a.salas@gmail.com", "calle mayor", "02-02-2000", 23);
